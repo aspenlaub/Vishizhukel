511d7e728699846d47a171f7039f02479c61b9fb
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-05-10 13:45:39 +0200

    Copy Foundry project structure

---
+++ solution.cake
@@ -0,0 +1,4 @@
+var solution = "./src/Vishizhukel.sln";
+var masterDebugBinFolder = "../VishizhukelBin/Debug";
+var masterReleaseBinFolder = "../VishizhukelBin/Release";
+var solutionSpecialSettingsDictionary = new Dictionary<string, string>();
---
+++ src/Test/ThisIsNotATest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test {
+    [TestClass]
+    public class ThisIsNotATest {
+        [TestMethod]
+        public void ThisIsNotATestTestMethod() {
+        }
+    }
+}
