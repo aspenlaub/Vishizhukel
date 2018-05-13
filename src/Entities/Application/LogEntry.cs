19a5adf8b0991dd6312cea67e73819710fe41a62
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-05-13 20:01:45 +0200

    Log entry class added

---
+++ src/Entities/Application/LogEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Interfaces.Basic;
+using Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Interfaces.Basic.Application;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Entities.Application {
+    public class LogEntry : IGuid, INotifyPropertyChanged, ILogEntry {
+        [Key]
+        public string Guid { get; set; }
+        private DateTime vCreatedAt;
+        public DateTime CreatedAt { get { return vCreatedAt; } set { vCreatedAt = value; OnPropertyChanged(nameof(CreatedAt)); } }
+        private LogEntryClass vClass;
+        public LogEntryClass Class { get { return vClass; } set { vClass = value; OnPropertyChanged(nameof(Class)); } }
+        private string vMessage;
+        public string Message { get { return vMessage; } set { vMessage = value; OnPropertyChanged(nameof(Message)); } }
+        private long vSequenceNumber;
+        public long SequenceNumber { get { return vSequenceNumber; } set { vSequenceNumber = value; OnPropertyChanged(nameof(SequenceNumber)); } }
+
+        public LogEntry() {
+            Guid = System.Guid.NewGuid().ToString();
+            vCreatedAt = DateTime.Now;
+            vClass = LogEntryClass.Information;
+            vMessage = "";
+            SequenceNumber = GlobalSequenceNumberGenerator.NextValue;
+        }
+
+        protected void OnPropertyChanged(string propertyName) {
+            // ReSharper disable once UseNullPropagation
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
