e8ecba42d90083f666a0f54496f91977863b4dbe
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2018-07-12 18:00:40 +0200

    Exception saver

---
+++ src/Core/ExceptionSaver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Core {
+    public class ExceptionSaver {
+        public static void SaveUnhandledException(Exception exception, string source, Action<SavedException> onExceptionSaved) {
+            var assemblyName = Assembly.GetExecutingAssembly().GetName();
+            var title = $"Unhandled exception in {assemblyName.Name} v{assemblyName.Version}";
+            var message = exception.Message;
+            var stackTrace = exception.StackTrace;
+            string innerMessage = "", innerStackTrace = "";
+            if (exception.InnerException != null) {
+                innerMessage = exception.InnerException.Message;
+                innerStackTrace = exception.InnerException.StackTrace;
+            }
+            SaveException(title, message, stackTrace, innerMessage, innerStackTrace, onExceptionSaved);
+        }
+
+        private static void SaveException(string title, string message, string stackTrace, string innerMessage, string innerStackTrace, Action<SavedException> onExceptionSaved) {
+            const string folder = @"c:\temp\Exceptions\";
+            if (!Directory.Exists(folder)) {
+                Directory.CreateDirectory(folder);
+            }
+
+            var fileContents = "Title:\r\n" + title + "\r\nMessage:\r\n" + message + "\r\nStack trace:\r\n" + stackTrace + "\r\nInner message:\r\n" + innerMessage + "\r\nInner stack trace:\r\n" + innerStackTrace;
+            var md5 = MD5.Create();
+            var inputBytes = Encoding.UTF8.GetBytes(fileContents);
+            var hash = md5.ComputeHash(inputBytes);
+            var sb = new StringBuilder();
+            foreach (var t in hash) { sb.Append(t.ToString("X2")); }
+
+            var exceptionName = "Exception-" + sb;
+            var fileName = folder + exceptionName + ".txt";
+            File.WriteAllText(fileName, fileContents);
+
+            var savedException = new SavedException {
+                ExceptionName = exceptionName,
+                FileContents = fileContents,
+                FileFullName = fileName
+            };
+            onExceptionSaved(savedException);
+        }
+    }
+
+    public class SavedException {
+        public string ExceptionName { get; set; }
+        public string FileContents { get; set; }
+        public string FileFullName { get; set; }
+    }
+}
