1c10097efb8ebcd14cba661dd8ed006e11c33026
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-05-04 11:41:59 +0200

    Delete ioc container. Move text file writer from Foundry project to here

---
+++ src/Core/TextFileWriter.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Core {
+    public class TextFileWriter {
+
+        public void WriteAllLines(IFolder folder, string utf8FileName, List<string> lines, Encoding encoding) {
+            var fileName = ReplaceInvalidCharacters(utf8FileName);
+            if (folder.FullName.Length < 5) { return; }
+
+            if (FileExistsAndIsIdentical(folder, fileName, lines, encoding)) { return; }
+
+            CreateBackupFile(folder, fileName);
+            var fullFileName = folder.FullName + '\\' + fileName;
+            File.WriteAllLines(fullFileName, lines, encoding);
+        }
+
+        public bool FileExistsAndIsIdentical(IFolder folder, string utf8FileName, List<string> lines, Encoding encoding) {
+            var fileName = ReplaceInvalidCharacters(utf8FileName);
+            var fullFileName = folder.FullName + '\\' + fileName;
+            if (!File.Exists(fullFileName)) { return false; }
+
+            var existingLines = File.ReadAllLines(fullFileName, encoding).ToList();
+            return existingLines.SequenceEqual(lines);
+        }
+
+        protected void CreateBackupFile(IFolder folder, string fileName) {
+            if (!File.Exists(folder.FullName + '\\' + fileName)) { return; }
+            if (!fileName.Contains('.')) { return; }
+
+            var fileNameWithoutExtension = fileName.Substring(0, fileName.LastIndexOf('.') + 1);
+            for (uint n = 1; n < 1000; n ++) {
+                var suffix = n.ToString("D3");
+                if (File.Exists(folder.FullName + '\\' + fileNameWithoutExtension + suffix)) {
+                    continue;
+                }
+
+                File.Copy(folder.FullName + '\\' + fileName, folder.FullName + '\\' + fileNameWithoutExtension + suffix);
+                return;
+            }
+        }
+
+        private static string ReplaceInvalidCharacters(string utf8FileName) {
+            return Path.GetInvalidFileNameChars().Aggregate(utf8FileName, (current, c) => current.Replace(c, '_'));
+        }
+    }
+}
---
+++ src/Test/Core/TextFileWriterTest.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Components;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Extensions;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Interfaces;
+using Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Core {
+    [TestClass]
+    public class TextFileWriterTest {
+        private readonly IComponentProvider vComponentProvider;
+
+        public TextFileWriterTest() {
+            vComponentProvider = new ComponentProvider();
+        }
+
+        internal bool AreTheseFilesInTheTestFolder(TextFileWriterTestExecutionContext context, List<string> shortFileNames) {
+            var existingFileNames = Directory.GetFiles(context.TestFolder.FullName, "*.*").OrderBy(x => x).ToList();
+            return existingFileNames.SequenceEqual(shortFileNames.Select(x => context.TestFolder.FullName + '\\' + x));
+        }
+
+        [TestMethod]
+        public void CanWriteAFile() {
+            using (var context = new TextFileWriterTestExecutionContext(vComponentProvider)) {
+                context.TextFileWriter.WriteAllLines(context.TestFolder, context.TestFileName, context.Lines, Encoding.UTF8);
+                Assert.IsTrue(File.Exists(context.TestFolder.FullName + '\\' + context.TestFileName));
+                Assert.IsTrue(AreTheseFilesInTheTestFolder(context, new List<string> { context.TestFileName }));
+                Assert.IsFalse(AreTheseFilesInTheTestFolder(context, new List<string> { context.TestFileName, context.TestFileName }));
+                Assert.IsFalse(AreTheseFilesInTheTestFolder(context, new List<string> { "", context.TestFileName }));
+                Assert.IsTrue(context.TextFileWriter.FileExistsAndIsIdentical(context.TestFolder, context.TestFileName, context.Lines, Encoding.UTF8));
+            }
+        }
+
+        [TestMethod]
+        public void OverwriteFileWithNewContentsCreatesBackupFile() {
+            using (var context = new TextFileWriterTestExecutionContext(vComponentProvider)) {
+                context.TextFileWriter.WriteAllLines(context.TestFolder, context.TestFileName, context.Lines, Encoding.UTF8);
+                Assert.IsTrue(AreTheseFilesInTheTestFolder(context, new List<string> { context.TestFileName }));
+                Assert.IsTrue(context.TextFileWriter.FileExistsAndIsIdentical(context.TestFolder, context.TestFileName, context.Lines, Encoding.UTF8));
+                Assert.IsFalse(context.TextFileWriter.FileExistsAndIsIdentical(context.TestFolder, context.TestFileName, context.ChangedLines, Encoding.UTF8));
+                context.TextFileWriter.WriteAllLines(context.TestFolder, context.TestFileName, context.ChangedLines, Encoding.UTF8);
+                Assert.IsTrue(AreTheseFilesInTheTestFolder(context, new List<string> { context.TestFileBackup1Name, context.TestFileName }));
+                Assert.IsTrue(context.TextFileWriter.FileExistsAndIsIdentical(context.TestFolder, context.TestFileName, context.ChangedLines, Encoding.UTF8));
+                Assert.IsTrue(context.TextFileWriter.FileExistsAndIsIdentical(context.TestFolder, context.TestFileBackup1Name, context.Lines, Encoding.UTF8));
+            }
+        }
+    }
+
+    internal class TextFileWriterTestExecutionContext : IDisposable {
+        internal TextFileWriter TextFileWriter { get; }
+        internal IFolder TestFolder { get; }
+        internal string TestFileName { get; }
+        internal string TestFileBackup1Name { get; }
+        internal List<string> Lines { get; }
+        internal List<string> ChangedLines { get; }
+
+        public TextFileWriterTestExecutionContext(IComponentProvider componentProvider) {
+            TextFileWriter = new TextFileWriter();
+            var errorsAndInfos = new ErrorsAndInfos();
+            TestFolder = new Folder(Path.GetTempPath()).SubFolder("AspenlaubTemp").SubFolder(nameof(TextFileWriterTest));
+            Assert.IsFalse(errorsAndInfos.AnyErrors(), errorsAndInfos.ErrorsToString());
+            TestFileName = "begin_end.txt";
+            TestFileBackup1Name = "begin_end.001";
+            Lines = new List<string> { "begin", "end" };
+            ChangedLines = new List<string>() { "/* V=100 */", "begin", "end" };
+            CleanUp();
+            Directory.CreateDirectory(TestFolder.FullName);
+        }
+
+        public void CleanUp() {
+            if (Directory.Exists(TestFolder.FullName)) {
+                new FolderDeleter().DeleteFolder(new Folder(TestFolder.FullName));
+            }
+        }
+
+        public void Dispose() {
+            CleanUp();
+        }
+    }
+}
