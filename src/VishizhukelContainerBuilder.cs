67eabb324a95ed5188c366bd82321f3f7dd52d19
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2021-04-21 22:48:22 +0200

    Something missing in the container builder

--- src/VishizhukelContainerBuilder.cs
+++ src/VishizhukelContainerBuilder.cs
@@ -13,6 +13,7 @@ using Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Interfaces.Web;
 using Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Web;
 using Autofac;
 using Microsoft.Extensions.DependencyInjection;
+// ReSharper disable UnusedMember.Global
 
 namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel {
     public static class VishizhukelContainerBuilder {
@@ -31,6 +32,44 @@ namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel {
                 builder.UsePegh(csArgumentPrompter);
             }
 
+            var securedHttpGateSettings = CreateSecuredHttpGateSettings(csArgumentPrompter);
+            builder.RegisterInstance<ISecuredHttpGateSettings>(securedHttpGateSettings);
+
+            builder.RegisterType<ApplicationLog>().As<IApplicationLog>();
+            builder.RegisterType<HttpGate>().As<IHttpGate>();
+            builder.RegisterType<SecuredHttpGate>().As<ISecuredHttpGate>();
+            builder.RegisterType<TextFileWriter>().As<ITextFileWriter>();
+            builder.RegisterType<WebFileSource>().As<IWebFileSource>();
+            return builder;
+        }
+
+        public static IServiceCollection UseVishizhukelAndPegh(this IServiceCollection services, ICsArgumentPrompter csArgumentPrompter) {
+            return UseVishizhukelAndPeghOptionallyDvin(services, csArgumentPrompter, false);
+        }
+
+        public static IServiceCollection UseVishizhukelDvinAndPegh(this IServiceCollection services, ICsArgumentPrompter csArgumentPrompter) {
+            return UseVishizhukelAndPeghOptionallyDvin(services, csArgumentPrompter, true);
+        }
+
+        private static IServiceCollection UseVishizhukelAndPeghOptionallyDvin(this IServiceCollection services, ICsArgumentPrompter csArgumentPrompter, bool useDvin) {
+            if (useDvin) {
+                services.UseDvinAndPegh(csArgumentPrompter);
+            } else {
+                services.UsePegh(csArgumentPrompter);
+            }
+
+            var securedHttpGateSettings = CreateSecuredHttpGateSettings(csArgumentPrompter);
+            services.AddSingleton<ISecuredHttpGateSettings>(securedHttpGateSettings);
+
+            services.AddTransient<IApplicationLog, ApplicationLog>();
+            services.AddTransient<IHttpGate, HttpGate>();
+            services.AddTransient<ISecuredHttpGate, SecuredHttpGate>();
+            services.AddTransient<ITextFileWriter, TextFileWriter>();
+            services.AddTransient<IWebFileSource, WebFileSource>();
+            return services;
+        }
+
+        private static SecuredHttpGateSettings CreateSecuredHttpGateSettings(ICsArgumentPrompter csArgumentPrompter) {
             var peghContainer = new ContainerBuilder().UsePegh(csArgumentPrompter).Build();
             var repository = peghContainer.Resolve<ISecretRepository>();
             var securedHttpGateSettingsSecret = new SecretSecuredHttpGateSettings();
@@ -40,14 +79,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel {
                 throw new Exception(errorsAndInfos.ErrorsToString());
             }
 
-            builder.RegisterInstance<ISecuredHttpGateSettings>(securedHttpGateSettings);
-
-            builder.RegisterType<ApplicationLog>().As<IApplicationLog>();
-            builder.RegisterType<HttpGate>().As<IHttpGate>();
-            builder.RegisterType<SecuredHttpGate>().As<ISecuredHttpGate>();
-            builder.RegisterType<TextFileWriter>().As<ITextFileWriter>();
-            builder.RegisterType<WebFileSource>().As<IWebFileSource>();
-            return builder;
+            return securedHttpGateSettings;
         }
     }
 }
