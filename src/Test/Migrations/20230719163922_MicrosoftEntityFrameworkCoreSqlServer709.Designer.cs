6a116cc5079feece380ee3d497cb50cddffa0c0b
VerBot <verbot@aspenlaub.net>
2023-07-19 18:40:25 +0200

    Package/-s updated to most recent version

--- src/Test/Migrations/20230719163922_MicrosoftEntityFrameworkCoreSqlServer709.Designer.cs
+++ src/Test/Migrations/20230719163922_MicrosoftEntityFrameworkCoreSqlServer709.Designer.cs
@@ -3,6 +3,7 @@ using Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations
 {
     [DbContext(typeof(TestContext))]
-    partial class TestContextModelSnapshot : ModelSnapshot
+    [Migration("20230719163922_MicrosoftEntityFrameworkCoreSqlServer709")]
+    partial class MicrosoftEntityFrameworkCoreSqlServer709
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
