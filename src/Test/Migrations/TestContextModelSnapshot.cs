b1f78e8e08eca55b0ab8ca94dba6f58e0d6a352a
VerBot <verbot@aspenlaub.net>
2024-01-10 10:46:36 +0100

    Package/-s updated to most recent version

--- src/Test/Migrations/TestContextModelSnapshot.cs
+++ src/Test/Migrations/TestContextModelSnapshot.cs
@@ -7,28 +7,32 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations {
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations
+{
     [DbContext(typeof(TestContext))]
-    partial class TestContextModelSnapshot : ModelSnapshot {
-        protected override void BuildModel(ModelBuilder modelBuilder) {
+    partial class TestContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.0")
+                .HasAnnotation("ProductVersion", "8.0.1")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Data.TestData", b => {
-                b.Property<string>("Guid")
-                    .HasColumnType("nvarchar(450)");
+            modelBuilder.Entity("Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Data.TestData", b =>
+                {
+                    b.Property<string>("Guid")
+                        .HasColumnType("nvarchar(450)");
 
-                b.Property<string>("Name")
-                    .HasColumnType("nvarchar(max)");
+                    b.Property<string>("Name")
+                        .HasColumnType("nvarchar(max)");
 
-                b.HasKey("Guid");
+                    b.HasKey("Guid");
 
-                b.ToTable("TestDatas");
-            });
+                    b.ToTable("TestDatas");
+                });
 #pragma warning restore 612, 618
         }
     }
