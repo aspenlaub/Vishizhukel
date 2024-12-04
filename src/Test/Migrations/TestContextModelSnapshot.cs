63603c75241d9a946dbeee1fac6999d425736250
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2024-12-04 08:37:15 +0100

    Update to .Net 9

--- src/Test/Migrations/TestContextModelSnapshot.cs
+++ src/Test/Migrations/TestContextModelSnapshot.cs
@@ -2,37 +2,31 @@
 using Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations
-{
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations {
     [DbContext(typeof(TestContext))]
-    partial class TestContextModelSnapshot : ModelSnapshot
-    {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
+    partial class TestContextModelSnapshot : ModelSnapshot {
+        protected override void BuildModel(ModelBuilder modelBuilder) {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.6")
+                .HasAnnotation("ProductVersion", "9.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Data.TestData", b =>
-                {
-                    b.Property<string>("Guid")
-                        .HasColumnType("nvarchar(450)");
+            modelBuilder.Entity("Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Data.TestData", b => {
+                b.Property<string>("Guid")
+                    .HasColumnType("nvarchar(450)");
 
-                    b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("Name")
+                    .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Guid");
+                b.HasKey("Guid");
 
-                    b.ToTable("TestDatas");
-                });
+                b.ToTable("TestDatas");
+            });
 #pragma warning restore 612, 618
         }
     }
