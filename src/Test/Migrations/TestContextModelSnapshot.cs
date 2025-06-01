036307ec0c4a33867acddc6bb7d344144bfc6fd7
VerBot <verbot@aspenlaub.net>
2025-06-01 20:12:16 +0200

    Package/-s updated to most recent version

--- src/Test/Migrations/TestContextModelSnapshot.cs
+++ src/Test/Migrations/TestContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.4")
+                .HasAnnotation("ProductVersion", "9.0.5")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
