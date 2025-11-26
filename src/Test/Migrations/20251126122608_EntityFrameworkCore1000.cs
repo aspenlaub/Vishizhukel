4e585d9c7c28ed3c5ae58de11980a70e1cd05f4c
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2025-11-26 13:27:20 +0100

    Package/-s updated to most recent version

---
+++ src/Test/Migrations/20251126122608_EntityFrameworkCore1000.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations {
+    /// <inheritdoc />
+    public partial class EntityFrameworkCore1000 : Migration {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) {
+
+        }
+    }
+}
--- src/Test/Migrations/TestContextModelSnapshot.cs
+++ src/Test/Migrations/TestContextModelSnapshot.cs
@@ -5,34 +5,28 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-#nullable disable
-
-namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations
-{
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations {
     [DbContext(typeof(TestContext))]
-    partial class TestContextModelSnapshot : ModelSnapshot
-    {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
+    partial class TestContextModelSnapshot : ModelSnapshot {
+        protected override void BuildModel(ModelBuilder modelBuilder) {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.11")
+                .HasAnnotation("ProductVersion", "10.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Data.TestData", b =>
-                {
-                    b.Property<string>("Guid")
-                        .HasColumnType("nvarchar(450)");
+            modelBuilder.Entity("Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Data.TestData", b => {
+                b.Property<string>("Guid")
+                    .HasColumnType("nvarchar(450)");
 
-                    b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)");
+                b.Property<string>("Name")
+                    .HasColumnType("nvarchar(max)");
 
-                    b.HasKey("Guid");
+                b.HasKey("Guid");
 
-                    b.ToTable("TestDatas");
-                });
+                b.ToTable("TestDatas");
+            });
 #pragma warning restore 612, 618
         }
     }
