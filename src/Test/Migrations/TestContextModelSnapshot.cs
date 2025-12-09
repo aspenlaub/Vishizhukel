80d22ed0729c361118f44a8fa19a9f11937db3c3
VerBot <verbot@aspenlaub.net>
2025-12-09 19:46:20 +0100

    Package/-s updated to most recent version

--- src/Test/Migrations/TestContextModelSnapshot.cs
+++ src/Test/Migrations/TestContextModelSnapshot.cs
@@ -5,28 +5,34 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations {
+#nullable disable
+
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations
+{
     [DbContext(typeof(TestContext))]
-    partial class TestContextModelSnapshot : ModelSnapshot {
-        protected override void BuildModel(ModelBuilder modelBuilder) {
+    partial class TestContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.0")
+                .HasAnnotation("ProductVersion", "10.0.1")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
-            modelBuilder.Entity("Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Data.TestData", b => {
-                b.Property<string>("Guid")
-                    .HasColumnType("nvarchar(450)");
+            modelBuilder.Entity("Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Data.TestData", b =>
+                {
+                    b.Property<string>("Guid")
+                        .HasColumnType("nvarchar(450)");
 
-                b.Property<string>("Name")
-                    .HasColumnType("nvarchar(max)");
+                    b.Property<string>("Name")
+                        .HasColumnType("nvarchar(max)");
 
-                b.HasKey("Guid");
+                    b.HasKey("Guid");
 
-                b.ToTable("TestDatas");
-            });
+                    b.ToTable("TestDatas");
+                });
 #pragma warning restore 612, 618
         }
     }
