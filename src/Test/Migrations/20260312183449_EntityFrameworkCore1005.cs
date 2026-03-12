e27c46f53b6011c414a257551940c006d325efa5
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2026-03-12 19:35:51 +0100

    Entity framework update

---
+++ src/Test/Migrations/20260312183449_EntityFrameworkCore1005.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aspenlaub.Net.GitHub.CSharp.Vishizhukel.Test.Migrations {
+    /// <inheritdoc />
+    public partial class EntityFrameworkCore1005 : Migration {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) {
+
+        }
+    }
+}
